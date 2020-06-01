1fd99f679de56d5173573d41c528e0af9c41319c
홍지성 <dgf5820@daum.net>
2020-06-01 13:42:02 +0900

    # Update - 사용자 다운로드 / 관리자 사이드메뉴 이벤트 수정

--- js/dashboard-admin.js
+++ js/dashboard-admin.js
@@ -1,8 +1,8 @@
-$(document).on('click', '.item', function(){
-  $('.item.on').removeClass('on');
+$(document).on('click', '.js-item', function(){
+  $('.js-item.on').removeClass('on');
   $(this).addClass('on');
 
-  var i = $(this).index();
+  var i = $(this).index()*1-1;
   $('.tab.on').removeClass('on');
   $('.tab').eq(i).addClass('on');
 })
--- js/dashboard-user.js
+++ js/dashboard-user.js
@@ -37,6 +37,7 @@ function loadPieChart (arr, ctx, vmData) {
   }));
 }
 
+// 공용 클라우드 마우스오버 이벤트
 $('#public-cloud .vm').mouseover(function(){
   var i = $(this).index()
   ChartArray1[i].data.datasets[0].backgroundColor = ['#E8A02588', '#AA54E888', '#3de78488'];
@@ -47,6 +48,11 @@ $('#public-cloud .vm').mouseout(function(){
   ChartArray1[i].data.datasets[0].backgroundColor = [];
   ChartArray1[i].update();
 })
+$('#public-cloud .chart').each(function(i, o) {
+  loadPieChart(ChartArray1, o, sample[i]);
+});
+
+// 사설 클라우드 마우스오버 이벤트
 $('#private-cloud .vm').mouseover(function(){
   var i = $(this).index()
   ChartArray2[i].data.datasets[0].backgroundColor = ['#E8A02588', '#AA54E888', '#3de78488'];
@@ -57,15 +63,11 @@ $('#private-cloud .vm').mouseout(function(){
   ChartArray2[i].data.datasets[0].backgroundColor = [];
   ChartArray2[i].update();
 })
-
-$('#public-cloud .chart').each(function(i, o) {
-  loadPieChart(ChartArray1, o, sample[i]);
-});
-
 $('#private-cloud .chart').each(function(i, o) {
   loadPieChart(ChartArray2, o, sample[i]);
 });
 
+// 사이드 메튜 클릭시 선택 이벤트
 $(document).on('click', '.item', function(){
   $('.item.on').removeClass('on');
   $(this).addClass('on');
@@ -75,3 +77,21 @@ $(document).on('click', '.item', function(){
   $('.tab').eq(i).addClass('on');
 })
 
+// VM 카드 클릭시 확대 이벤트
+$('#public-cloud .vm').on('click', function(){
+  $(this).off('mouseout');
+  $(this).off('mouseover');
+
+  // chart color
+  var i = $(this).index()
+  ChartArray2[i].data.datasets[0].backgroundColor = ['#E8A02588', '#AA54E888', '#3de78488'];
+  ChartArray2[i].update();
+
+  // text color
+  $('#public-cloud .vm.on').removeClass('on');
+  $(this).addClass('on');
+  
+  // big size
+  $('#public-cloud .vm').removeClass('detail');
+  $(this).addClass('detail');
+})
